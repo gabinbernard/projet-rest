abe60f54ab7d5ef64b490406beeefd82ee8e038c
Gabin BERNARD <gbernard@smart-traffik.io>
2024-10-31 16:26:32 +0100

    fix: better log, update specification.json

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -1,5 +1,6 @@
 export function log(msg) {
-    console.log(`[ API ]${msg.length > 0 ? ' - ' : ''}${msg}`);
+    const time = new Date().getTime();
+    console.log(`[ API | ${time} ]${msg && msg.length > 0 ? ` ${msg}` : ''}`);
 }
 
 export default log;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./specification.json');
 const { default: log } = require('./helpers/log');
 
+const PORT = 3000;
+
 app.use('/reference/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(3000, () => {
+app.listen(PORT, () => {
+    log();
     log("Server running on port 3000!");
-    log("");
+    log();
     log("API Root - http://localhost:3000");
     log("Reference - http://localhost:3000/reference");
+    log();
+    log("----------------------------------------------");
+    log();
+
 })
